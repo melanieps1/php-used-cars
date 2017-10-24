36044ba51a50cf20253a9682886ed9426a509bb3
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-10-24 16:13:42 -0400

    pulled database info in and made a bootstrap table out of it for the cars

--- index.php
+++ index.php
@@ -10,17 +10,67 @@
 <body class="container">
 
 	<?php
-		$name = 'Melanie';
-		$day_of_week = 'Tuesday';
-		$daily_discount = '10%';
+		
+		function getDb() {
+			// `pg` in `pg_connect` stands for Postgres
+			$db = pg_connect("host=localhost port=5432 dbname=cars_dev user=carsuser password=carscarscars");
+			return $db;
+		}
+
+		function getInventory() {
+			// Creating a request for fetching the data
+			$request = pg_query(getDb(), "
+				SELECT i.id, i.year, i.mileage, ma.name as make, mo.name as model, mo.doors, c.name as color
+				FROM inventory i
+				JOIN models mo ON i.model_id = mo.id
+				JOIN makes ma ON mo.make_id = ma.id
+				JOIN colors c ON i.color_id = c.id;
+			");
+			// Now getting all of the records in one chunk - this is a pattern that we will repeat a lot!!!
+			return pg_fetch_all($request);
+		}
+
 	?>
 
 	<h1>PHP Used Car Website</h1>
 	<h2>Quality pre-owned vehicles, powered by PHP!</h2>
+	<br>
+
+	<table class="table">
+		<tr>
+			<th>ID</th>
+			<th>Year</th>
+			<th>Mileage</th>
+			<th>Make</th>
+			<th>Model</th>
+			<th>Doors</th>
+			<th>Color</th>
+		</tr>
 
 	<?php
-		echo "Welcome!  My name is $name.  Our $day_of_week discount is $daily_discount off today.";
-	?>
+
+	// var_dump(getinventory());
+
+	// This foreach loop is cycling through the whole chunk of data we are getting back from the query and going through it line by line
+		foreach (getInventory() as $car) {
+			// var_dump($car);
+			// $car is an associative array
+
+			echo "<tr>";
+
+		// This foreach loop is going through the data field by field
+			foreach ($car as $field => $value) {
+				// echo "$field is $value\n";
+				echo "<td>$value</td>";
+			}
+
+			echo "</tr>\n";
+
+		}
+
+		?>
+
+	</table>
 
 </body>
 </html>
